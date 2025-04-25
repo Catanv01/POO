9977c6727dc1c065c354966afbb1c1dafe647d31
Catalina Nunez Valencia <1107843064@u.icesi.edu.co>
2025-04-25 16:32:45 -0500

    Practica ejemplo

---
+++ script.js
@@ -0,0 +1,12 @@
+const Cata = {
+    name: "Juan",
+    lastName: "Perez",
+    age: 19,
+    country: "Colombia",
+    profession: "Designer",
+}
+
+console.log(Cata.age)
+
+Cata.isMarried = false
+Cata.hasChildren = false
